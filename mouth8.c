b6937e36214ae5ec7c32e3304b6f55311c6d8ab5
lWolvesl <64883691+lWolvesl@users.noreply.github.com>
2022-08-14 19:03:12 +0800

    reademe

--- mouth8.c
+++ mouth8.c
@@ -2,10 +2,8 @@
 // Created by wolves on 2022/8/14.
 //
 
-#include <string.h>
-#include "stdlib.h"
+#include "string.h"
 #include "stdio.h"
-#include "math.h"
 
 /**
  * 给你一个由若干 0 和 1 组成的字符串 s ，请你计算并返回将该字符串分割成两个 非空 子字符串（即 左 子字符串和 右 子字符串）所能获得的最大得分。
@@ -17,17 +15,18 @@ int day14(char *s) {
     int max = 0;
     int index = 0;
     int length = strlen(s);
-    printf("%d\n", length);
     while (index < length - 1) {
         int pear = 0;
         int rear = 0;
-        for (int i = index; i >= 0; i--) {
-            int x = pow(2, index - i);
-            pear += s[i] * x;
+        for (int i = 0; i <= index; i++) {
+            if (s[i] == '0') {
+                pear++;
+            }
         }
-        for (int i = length - 1; i > index; i++) {
-            int x = pow(2, index - index - i - 1);
-            rear += s[i] * x;
+        for (int i = index + 1; i < length; i++) {
+            if (s[i] == '1') {
+                rear++;
+            }
         }
         ++index;
         max = max < pear + rear ? pear + rear : max;
@@ -36,6 +35,6 @@ int day14(char *s) {
 }
 
 void day14Run() {
-    char *a = "011101";
+    char *a = "00";
     printf("%d\n", day14(a));
 }
