5a24fd20ed4fc9e15374efddfa47c1c3f9dc7bdd
lWolvesl <64883691+lWolvesl@users.noreply.github.com>
2022-08-19 18:44:37 +0800

    routine

--- year22/mouth8/mouth8.c
+++ year22/mouth8/mouth8.c
@@ -38,3 +38,37 @@ void day14Run() {
     char *a = "00";
     printf("%d\n", day14(a));
 }
+
+/**
+ * https://leetcode.cn/problems/deepest-leaves-sum/
+ */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int day18(struct TreeNode* root){
+
+}
+
+/**
+ * https://leetcode.cn/problems/number-of-students-doing-homework-at-a-given-time/
+ * 给你两个整数数组 startTime（开始时间）和 endTime（结束时间），并指定一个整数 queryTime 作为查询时间。
+
+已知，第 i 名学生在 startTime[i] 时开始写作业并于 endTime[i] 时完成作业。
+
+请返回在查询时间 queryTime 时正在做作业的学生人数。形式上，返回能够使 queryTime 处于区间 [startTime[i], endTime[i]]（含）的学生人数。
+ * @return
+ */
+int day19(int* startTime, int startTimeSize, int* endTime, int endTimeSize, int queryTime){
+    int count = 0;
+    for (int i = 0; i < startTimeSize; ++i) {
+        if(startTime[i]<=queryTime){
+            if(endTime[i]>=queryTime){
+                count++;
+            }
+        }
+    }
+    return count;
+}
